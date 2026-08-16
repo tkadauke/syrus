1cf133be303982942811d11dc9325452828fa63d
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-08-16 11:27:31 -0400

    Batch observability log retention deletes

--- app/jobs/flush_observability_events_job.rb
+++ app/jobs/flush_observability_events_job.rb
@@ -1,9 +1,26 @@
 class FlushObservabilityEventsJob < ApplicationJob
   queue_as :low_priority_maintenance
 
+  DELETE_BATCH_SIZE = Integer(ENV["SYRUS_OBSERVABILITY_PRUNE_BATCH_SIZE"], exception: false) || 1_000
+  MAX_DELETE_BATCHES = Integer(ENV["SYRUS_OBSERVABILITY_PRUNE_MAX_BATCHES"], exception: false) || 50
+
   def perform
     Observability::EventSink.flush!
-    PerformanceLogEvent.expired.delete_all
-    WorkflowActivityEvent.expired.delete_all
+    prune_expired(PerformanceLogEvent)
+    prune_expired(WorkflowActivityEvent)
+  end
+
+  private
+
+  def prune_expired(model)
+    deleted = 0
+    MAX_DELETE_BATCHES.times do
+      batch_deleted = model.expired.order(:id).limit(DELETE_BATCH_SIZE).delete_all
+      break if batch_deleted.zero?
+
+      deleted += batch_deleted
+    end
+    Rails.logger.info("[FlushObservabilityEventsJob] pruned #{deleted} #{model.name} rows") if deleted.positive?
+    deleted
   end
 end
--- spec/jobs/flush_observability_events_job_spec.rb
+++ spec/jobs/flush_observability_events_job_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe FlushObservabilityEventsJob do
     expect(PerformanceLogEvent.exists?(stale.id)).to eq(false)
     expect(PerformanceLogEvent.exists?(fresh.id)).to eq(true)
   end
+
+  it "bounds expired log deletes per run" do
+    stub_const("FlushObservabilityEventsJob::DELETE_BATCH_SIZE", 2)
+    stub_const("FlushObservabilityEventsJob::MAX_DELETE_BATCHES", 1)
+    stale = 3.times.map { PerformanceLogEvent.create!(occurred_at: 7.hours.ago, event_name: "sql", payload: {}) }
+    allow(Observability::EventSink).to receive(:flush!)
+
+    described_class.new.perform
+
+    remaining_ids = PerformanceLogEvent.where(id: stale.map(&:id)).pluck(:id)
+    expect(remaining_ids.size).to eq(1)
+  end
 end
