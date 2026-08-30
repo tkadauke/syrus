df4fb7424891318cfec5bc490a9e10b824d9df1a
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-08-30 19:45:37 -0400

    Remove global adversarial review spec stub

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -81,12 +81,6 @@ RSpec.configure do |config|
     # run's logs start numbering above 0.
     JobLog.clear_sequence_cache!
     Steps::AutoMerge.mergeability_settle_delay = 0
-    # Stub the GitHub API call made by RepoAdversarialReviewPlan#resolve so
-    # that specs using Factories.job (which instantiates an Initial workflow)
-    # don't silently rely on rescue StandardError catching a WebMock error.
-    # Specs that need adversarial review enabled override this in a local before.
-    allow(RepoAdversarialReviewPlan).to receive(:for_job)
-      .and_return(RepoAdversarialReviewPlan::Result.new(rounds: 0, source: "none", note: "no .syrus.yml", criteria: []))
   end
 
   config.after do
