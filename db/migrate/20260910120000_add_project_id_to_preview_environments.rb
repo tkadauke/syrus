3992527b2e97d17ac0fa14fcf2f20887b36000b1
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-10 06:50:46 -0400

    Support project-scoped previews

--- db/migrate/20260910120000_add_project_id_to_preview_environments.rb
+++
@@ -1,9 +0,0 @@
-class AddProjectIdToPreviewEnvironments < ActiveRecord::Migration[8.1]
-  def change
-    add_column :preview_environments, :project_id, :string unless column_exists?(:preview_environments, :project_id)
-    unless index_exists?(:preview_environments, [ :job_id, :project_id, :created_at, :id ], name: "idx_preview_environments_job_project_latest")
-      add_index :preview_environments, [ :job_id, :project_id, :created_at, :id ],
-        name: "idx_preview_environments_job_project_latest"
-    end
-  end
-end
