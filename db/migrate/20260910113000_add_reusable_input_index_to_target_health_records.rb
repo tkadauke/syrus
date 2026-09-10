56c48d4403e9c61004e69d3d0142be49e1319f0c
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-10 07:33:20 -0400

    Index reusable target health lookups

---
+++ db/migrate/20260910113000_add_reusable_input_index_to_target_health_records.rb
@@ -0,0 +1,18 @@
+class AddReusableInputIndexToTargetHealthRecords < ActiveRecord::Migration[8.1]
+  def change
+    columns = [
+      :repository_id,
+      :target_label,
+      :input_fingerprint,
+      :command_fingerprint,
+      :environment_fingerprint,
+      :checked_at,
+      :created_at
+    ]
+
+    add_index :target_health_records,
+      columns,
+      name: "idx_target_health_records_reusable_inputs",
+      if_not_exists: true
+  end
+end
