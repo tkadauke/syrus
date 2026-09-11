eb18fdd1014726d7017f9109063652b76805ee61
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-11 19:48:38 -0400

    Make GitHub API usage migration idempotent

--- db/migrate/20260911223000_create_github_api_usage_rollups.rb
+++ db/migrate/20260911223000_create_github_api_usage_rollups.rb
@@ -1,30 +1,42 @@
 class CreateGithubApiUsageRollups < ActiveRecord::Migration[8.1]
-  def change
-    create_table :github_api_usage_rollups do |t|
-      t.datetime :bucket_started_at, null: false
-      t.string :auth_source, null: false
-      t.string :credential_key, null: false
-      t.references :installation, foreign_key: true
-      t.references :user, foreign_key: true
-      t.references :repository, foreign_key: true
-      t.string :repository_key, null: false
-      t.string :repo_slug
-      t.string :operation, null: false
-      t.string :resource
-      t.integer :request_count, null: false, default: 0
-      t.integer :rate_limited_count, null: false, default: 0
-      t.integer :last_status
-      t.integer :last_remaining
-      t.integer :last_limit
-      t.datetime :last_reset_at
-      t.datetime :last_seen_at, null: false
+  INDEX_NAME = "idx_github_api_usage_rollups_unique_bucket"
 
-      t.timestamps
+  def up
+    unless table_exists?(:github_api_usage_rollups)
+      create_table :github_api_usage_rollups do |t|
+        t.datetime :bucket_started_at, null: false
+        t.string :auth_source, null: false
+        t.string :credential_key, null: false
+        t.references :installation, foreign_key: true
+        t.references :user, foreign_key: true
+        t.references :repository, foreign_key: true
+        t.string :repository_key, null: false
+        t.string :repo_slug
+        t.string :operation, null: false
+        t.string :resource
+        t.integer :request_count, null: false, default: 0
+        t.integer :rate_limited_count, null: false, default: 0
+        t.integer :last_status
+        t.integer :last_remaining
+        t.integer :last_limit
+        t.datetime :last_reset_at
+        t.datetime :last_seen_at, null: false
+
+        t.timestamps
+      end
     end
 
     add_index :github_api_usage_rollups,
       [ :bucket_started_at, :auth_source, :credential_key, :repository_key, :operation, :resource ],
       unique: true,
-      name: "idx_github_api_usage_rollups_unique_bucket"
+      name: INDEX_NAME,
+      if_not_exists: true
+    add_foreign_key :github_api_usage_rollups, :installations, if_not_exists: true
+    add_foreign_key :github_api_usage_rollups, :repositories, if_not_exists: true
+    add_foreign_key :github_api_usage_rollups, :users, if_not_exists: true
+  end
+
+  def down
+    drop_table :github_api_usage_rollups, if_exists: true
   end
 end
