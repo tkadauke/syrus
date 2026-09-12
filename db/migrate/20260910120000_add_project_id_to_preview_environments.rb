4be15176d589d48249ff5cec4e83d0acfe4da073
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-12 06:43:55 +0000

    Syrus merge-train reconciliation

--- db/migrate/20260910120000_add_project_id_to_preview_environments.rb
+++
@@ -1,8 +0,0 @@
-class AddProjectIdToPreviewEnvironments < ActiveRecord::Migration[8.1]
-  def change
-    add_column :preview_environments, :project_id, :string unless column_exists?(:preview_environments, :project_id)
-    add_index :preview_environments, [ :job_id, :project_id, :created_at, :id ],
-      name: "idx_preview_environments_job_project_latest",
-      if_not_exists: true
-  end
-end
