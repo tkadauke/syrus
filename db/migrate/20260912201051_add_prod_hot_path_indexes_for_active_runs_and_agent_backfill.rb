305bca501c1af725f0896a62491337dd600722aa
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-12 23:01:36 -0400

    Make hot path index migration idempotent

--- db/migrate/20260912201051_add_prod_hot_path_indexes_for_active_runs_and_agent_backfill.rb
+++ db/migrate/20260912201051_add_prod_hot_path_indexes_for_active_runs_and_agent_backfill.rb
@@ -1,18 +1,24 @@
 class AddProdHotPathIndexesForActiveRunsAndAgentBackfill < ActiveRecord::Migration[8.1]
   def change
-    add_index :runs,
-      [ :finished_at, :created_at, :id ],
-      name: "idx_runs_active_created",
-      if_not_exists: true
+    unless index_exists?(:runs, [ :finished_at, :created_at, :id ], name: "idx_runs_active_created")
+      add_index :runs,
+        [ :finished_at, :created_at, :id ],
+        name: "idx_runs_active_created",
+        if_not_exists: true
+    end
 
-    add_index :spawned_processes,
-      [ :agent_id, :run_id ],
-      name: "idx_spawned_processes_agent_run_backfill",
-      if_not_exists: true
+    unless index_exists?(:spawned_processes, [ :agent_id, :run_id ], name: "idx_spawned_processes_agent_run_backfill")
+      add_index :spawned_processes,
+        [ :agent_id, :run_id ],
+        name: "idx_spawned_processes_agent_run_backfill",
+        if_not_exists: true
+    end
 
-    add_index :spawned_processes,
-      [ :agent_id, :chat_session_id ],
-      name: "idx_spawned_processes_agent_chat_backfill",
-      if_not_exists: true
+    unless index_exists?(:spawned_processes, [ :agent_id, :chat_session_id ], name: "idx_spawned_processes_agent_chat_backfill")
+      add_index :spawned_processes,
+        [ :agent_id, :chat_session_id ],
+        name: "idx_spawned_processes_agent_chat_backfill",
+        if_not_exists: true
+    end
   end
 end
