06cd61483034c2ee71a1e658a419541720f78259
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-13 08:32:04 +0000

    Syrus merge-train reconciliation

--- plugins/global_search/lib/global_search/source.rb
+++ plugins/global_search/lib/global_search/source.rb
@@ -26,7 +26,8 @@ module GlobalSearch
   # A provider may also contribute a global-search result type:
   #
   #   def self.search_type = "my_thing"
-  #   def self.search(query:, user:, limit:) = [ { id:, title:, path: } ]
+  #   def self.search_rows(query:, user:, limit:) = [ { my_thing_id:, rank:, snippet: } ]
+  #   def self.result_json(row:, user:) = { type:, id:, title:, path:, snippet:, rank: }
   # The contract for a search result type contributed through this plugin's
   # "global_search:source" point.
   #
