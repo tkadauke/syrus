9971b29515f7d1e3c2560711605c10a8b1048691
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-12 22:32:07 -0400

    Route search rebuild through global search sources

--- plugins/global_search/app/services/global_search/epic_search_source.rb
+++ plugins/global_search/app/services/global_search/epic_search_source.rb
@@ -10,7 +10,7 @@ module GlobalSearch
     def self.search_backfill_total_count = Epic.count
 
     def self.search_backfill_needed?
-      indexed_count < Epic.count
+      indexed_count(TABLE_NAME, ID_COLUMN) < Epic.count
     rescue StandardError
       Epic.exists?
     end
@@ -28,10 +28,29 @@ module GlobalSearch
       { processed: processed, last_id: last_id, done: processed.zero? || last_id >= Epic.maximum(:id).to_i }
     end
 
-    def self.indexed_count
-      return 0 unless SyrusSearchDatabaseTasks.table_exists?(TABLE_NAME)
+    def self.search_database_rebuild_key = "epics"
+    def self.search_database_rebuild_units = Epic.count
 
-      SearchRecord.connection.select_value("SELECT COUNT(DISTINCT #{ID_COLUMN}) FROM #{TABLE_NAME}").to_i
+    def self.search_database_rebuild_pending?
+      search_backfill_needed?
     end
+
+    def self.search_database_rebuild_batch(task:)
+      task.current_step_key = "epics"
+      task.current_step_title = "Index epics"
+      batch = search_backfill_batch(after_id: task.checkpoint["last_epic_id"].to_i, limit: task.batch_size)
+      task.checkpoint_will_change!
+      task.checkpoint["last_epic_id"] = batch[:last_id] if batch[:last_id].present?
+      task.checkpoint["epics_done"] = true if batch[:done]
+
+      MaintenanceTasks::Definitions::Base::Result.new(done: false, processed: batch[:processed].to_i, failed: 0, message: "Indexed #{batch[:processed].to_i} epic(s).", level: "progress")
+    end
+
+    def self.indexed_count(table, id_column)
+      return 0 unless SyrusSearchDatabaseTasks.table_exists?(table)
+
+      SearchRecord.connection.select_value("SELECT COUNT(DISTINCT #{id_column}) FROM #{table}").to_i
+    end
+    private_class_method :indexed_count
   end
 end
