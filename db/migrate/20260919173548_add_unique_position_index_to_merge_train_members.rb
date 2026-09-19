6e91aac39a4af90d993b41f93efcbfa7af81decf
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-19 17:38:54 -0400

    Enforce merge train member ordering uniqueness

---
+++ db/migrate/20260919173548_add_unique_position_index_to_merge_train_members.rb
@@ -0,0 +1,25 @@
+class AddUniquePositionIndexToMergeTrainMembers < ActiveRecord::Migration[8.1]
+  INDEX_NAME = "index_merge_train_members_on_merge_train_id_and_position"
+
+  def up
+    if index_exists?(:merge_train_members, [ :merge_train_id, :position ], name: INDEX_NAME) &&
+        !index_exists?(:merge_train_members, [ :merge_train_id, :position ], name: INDEX_NAME, unique: true)
+      remove_index :merge_train_members, name: INDEX_NAME
+    end
+
+    add_index :merge_train_members,
+      [ :merge_train_id, :position ],
+      unique: true,
+      name: INDEX_NAME,
+      if_not_exists: true
+  end
+
+  def down
+    remove_index :merge_train_members, name: INDEX_NAME if index_exists?(:merge_train_members, name: INDEX_NAME)
+
+    add_index :merge_train_members,
+      [ :merge_train_id, :position ],
+      name: INDEX_NAME,
+      if_not_exists: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_19_120000) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_19_173548) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -1844,7 +1844,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_19_120000) do
     t.index ["job_id", "merge_train_id"], name: "idx_merge_train_members_job_train"
     t.index ["job_id"], name: "index_merge_train_members_on_job_id"
     t.index ["merge_train_id", "job_id"], name: "index_merge_train_members_on_merge_train_id_and_job_id", unique: true
-    t.index ["merge_train_id", "position"], name: "index_merge_train_members_on_merge_train_id_and_position"
+    t.index ["merge_train_id", "position"], name: "index_merge_train_members_on_merge_train_id_and_position", unique: true
     t.index ["merge_train_id"], name: "index_merge_train_members_on_merge_train_id"
   end
 
