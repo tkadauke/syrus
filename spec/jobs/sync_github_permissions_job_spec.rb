4108aa598081f0c1f4310e7dc859377df2236298
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-19 17:35:19 -0400

    Schedule GitHub permission sync

--- spec/jobs/sync_github_permissions_job_spec.rb
+++ spec/jobs/sync_github_permissions_job_spec.rb
@@ -11,6 +11,24 @@ RSpec.describe SyncGithubPermissionsJob do
     expect(syncer).to have_received(:sync)
   end
 
+  it "stamps repository membership mismatch fields when run from the scheduler" do
+    AppSetting.current.update!(github_app_id: 42)
+    owner = Factories.user(github_handle: "owner-handle")
+    installation = Factories.installation(user: owner)
+    repository = Factories.repository(user: owner, owner: "acme", name: "widgets", installation: installation)
+    client = instance_double(GithubClient)
+    allow(GithubClient).to receive(:for).with(repository: repository).and_return(client)
+    allow(client).to receive(:collaborator_permissions).with(repository.slug).and_return([])
+
+    described_class.perform_now
+
+    membership = repository.repository_memberships.find_by!(user: owner)
+    expect(membership.reload).to have_attributes(
+      github_permission_mismatch_reason: "not_a_github_collaborator",
+      github_permission_mismatch_checked_at: be_present
+    )
+  end
+
   it "no-ops when the GitHub App is not registered" do
     expect(GithubPermissionSyncer).not_to receive(:new)
     described_class.perform_now
