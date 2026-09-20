383f9dd4e021e2441f412a2c2f3830b77656fd81
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-19 20:09:46 -0400

    Fix merge train member index migration idempotency

--- db/migrate/20260919173548_add_unique_position_index_to_merge_train_members.rb
+++ db/migrate/20260919173548_add_unique_position_index_to_merge_train_members.rb
@@ -7,19 +7,21 @@ class AddUniquePositionIndexToMergeTrainMembers < ActiveRecord::Migration[8.1]
       remove_index :merge_train_members, name: INDEX_NAME
     end
 
-    add_index :merge_train_members,
-      [ :merge_train_id, :position ],
-      unique: true,
-      name: INDEX_NAME,
-      if_not_exists: true
+    unless index_exists?(:merge_train_members, [ :merge_train_id, :position ], name: INDEX_NAME)
+      add_index :merge_train_members,
+        [ :merge_train_id, :position ],
+        unique: true,
+        name: INDEX_NAME
+    end
   end
 
   def down
     remove_index :merge_train_members, name: INDEX_NAME if index_exists?(:merge_train_members, name: INDEX_NAME)
 
-    add_index :merge_train_members,
-      [ :merge_train_id, :position ],
-      name: INDEX_NAME,
-      if_not_exists: true
+    unless index_exists?(:merge_train_members, [ :merge_train_id, :position ], name: INDEX_NAME)
+      add_index :merge_train_members,
+        [ :merge_train_id, :position ],
+        name: INDEX_NAME
+    end
   end
 end
