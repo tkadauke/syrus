97bd8e58e6674d9fc9ffe929dda40520d6efb897
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-24 09:26:56 -0400

    Harden focused RSpec rerun command

--- plugins/ruby/lib/ruby/focused_test_command.rb
+++ plugins/ruby/lib/ruby/focused_test_command.rb
@@ -22,7 +22,11 @@ module Ruby
       files = failed_spec_files
       return nil if files.empty?
 
-      "bundle exec rspec #{Shellwords.join(files)}"
+      [
+        "mkdir -p .syrus/grade-output .syrus/rspec-json",
+        "if [ -x bin/rails ] && [ -f config/database.yml ]; then bin/rails db:test:prepare; fi",
+        "RAILS_ENV=${RAILS_ENV:-test} COVERAGE=false bundle exec rspec #{Shellwords.join(files)}"
+      ].join(" && ")
     end
 
     private
