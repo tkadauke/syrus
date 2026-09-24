0fca28d189760d27baaa6c511e3f75fafa5df0ac
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-24 09:19:52 -0400

    Fix retry enqueuer invalid reopen error

--- plugins/ruby/lib/ruby/focused_test_command.rb
+++ plugins/ruby/lib/ruby/focused_test_command.rb
@@ -3,6 +3,9 @@ require "shellwords"
 module Ruby
   class FocusedTestCommand
     include Syrus::Plugin::FocusedTestCommand
+    RAILS_ENV_PREFIX = "RAILS_ENV=test".freeze
+    RAILS_TEST_PREPARE_COMMAND = "if [ -x bin/rails ] && [ -f config/database.yml ]; " \
+      "then #{RAILS_ENV_PREFIX} bin/rails db:test:prepare; fi".freeze
 
     def self.command_for(grader_name:, grader_command:, failed_cases:, base_retry:)
       new(grader_name: grader_name, grader_command: grader_command, failed_cases: failed_cases, base_retry: base_retry).command
@@ -28,7 +31,10 @@ module Ruby
       files = failed_spec_files
       return nil if files.empty?
 
-      "RUN_CI_ONLY_SPECS=false COVERAGE=false bundle exec rspec --tag #{Shellwords.escape('~ci_only')} #{Shellwords.join(files)}"
+      [
+        RAILS_TEST_PREPARE_COMMAND,
+        "#{RAILS_ENV_PREFIX} RUN_CI_ONLY_SPECS=false COVERAGE=false bundle exec rspec --tag #{Shellwords.escape('~ci_only')} #{Shellwords.join(files)}"
+      ].join(" && ")
     end
 
     private
