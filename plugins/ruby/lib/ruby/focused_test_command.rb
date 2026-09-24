10050c3ab2bfc2ded5e2e4bfab7f07ab85e70e98
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-24 12:24:38 -0400

    Avoid Rails prepare in focused RSpec reruns

--- plugins/ruby/lib/ruby/focused_test_command.rb
+++ plugins/ruby/lib/ruby/focused_test_command.rb
@@ -4,8 +4,6 @@ module Ruby
   class FocusedTestCommand
     include Syrus::Plugin::FocusedTestCommand
     RAILS_ENV_PREFIX = "RAILS_ENV=test".freeze
-    RAILS_TEST_PREPARE_COMMAND = "if [ -x bin/rails ] && [ -f config/database.yml ]; " \
-      "then #{RAILS_ENV_PREFIX} bin/rails db:test:prepare; fi".freeze
 
     def self.command_for(grader_name:, grader_command:, failed_cases:, base_retry:)
       new(grader_name: grader_name, grader_command: grader_command, failed_cases: failed_cases, base_retry: base_retry).command
@@ -25,10 +23,7 @@ module Ruby
       files = failed_spec_files
       return nil if files.empty?
 
-      [
-        RAILS_TEST_PREPARE_COMMAND,
-        "#{RAILS_ENV_PREFIX} bundle exec rspec #{Shellwords.join(files)}"
-      ].join(" && ")
+      "#{RAILS_ENV_PREFIX} bundle exec rspec #{Shellwords.join(files)}"
     end
 
     private
