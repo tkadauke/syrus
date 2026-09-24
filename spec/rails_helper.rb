607836f681251c0a7fea2574b3391dcbaf3dd33a
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-24 13:58:11 -0400

    Fix WIP repair failure backfill detector

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,10 +28,7 @@ end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-# RSpec must never inherit the app server/worker environment. Syrus repeat
-# gates invoke file-scoped specs from production workers, so defaulting with
-# ||= would boot the application against the wrong database.
-ENV['RAILS_ENV'] = 'test'
+ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
