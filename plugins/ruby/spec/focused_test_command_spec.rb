4e0010fd55b130809b41e56b1667ca2df804b1f8
Thomas Kadauke <thomas.kadauke@googlemail.com>
2026-09-24 23:57:28 -0400

    Force RSpec focused repeats into test env

--- plugins/ruby/spec/focused_test_command_spec.rb
+++ plugins/ruby/spec/focused_test_command_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Ruby::FocusedTestCommand do
       base_retry: { "strategy" => "plugin" }
     )
 
-    expect(command).to eq("RUN_CI_ONLY_SPECS=false COVERAGE=false bundle exec rspec --tag \\~ci_only spec/models/widget_spec.rb")
+    expect(command).to eq("RAILS_ENV=test RUN_CI_ONLY_SPECS=false COVERAGE=false bundle exec rspec --tag \\~ci_only spec/models/widget_spec.rb")
   end
 
   it "declines when the grader did not opt into plugin strategy" do
